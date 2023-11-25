904ca6278f016d6e6c94c3b6f41393885821708f
Mrlion <mrlion4202@gmail.com>
2023-11-25 17:20:06 +0100

    changes

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,7 @@ new CommandKit({
   commandsPath: path.join(__dirname, 'commands'),
   eventsPath: path.join(__dirname, 'events'),
   validationsPath: path.join(__dirname, 'validations'),
-  devGuildIds: ['DEV_SERVER_ID_1', 'DEV_SERVER_ID_2'],
-  devUserIds: ['DEV_USER_ID_1', 'DEV_USER_ID_2'],
-  devRoleIds: ['DEV_ROLE_ID_1', 'DEV_ROLE_ID_2'],
+  devUserIds: ['710549603216261141'],
   skipBuiltInValidations: true,
   bulkRegister: true
 })
