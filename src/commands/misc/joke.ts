910f0bb6bedf97f729b7a734840a1d552e85f66b
Mrlion <mrlion4202@gmail.com>
2023-11-25 18:35:05 +0100

    some changes for this command

--- src/commands/misc/joke.ts
+++ src/commands/misc/joke.ts
@@ -6,23 +6,3 @@ export const data: CommandData = {
   name: 'joke',
   description: '🐸 Replies with some funny joke!'
 }
-
-export async function execute({ interaction }: SlashCommandProps) {
-  try {
-    const response = await fetch(icanhazdadjokeAPI)
-    const jokeData = await response.json()
-
-    const joke = jokeData.joke
-
-    await interaction.reply({
-      content: joke,
-      ephemeral: false
-    })
-  } catch (error) {
-    console.error('Failed to fetch joke:', error)
-    await interaction.reply({
-      content: 'Oops, something went wrong while fetching a joke.',
-      ephemeral: false
-    })
-  }
-}
