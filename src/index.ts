244945956e40cf71bd706f402abc33f45d0acbc1
Mrlion <mrlion4202@gmail.com>
2023-11-25 17:13:11 +0100

    added command kit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 import { Client, IntentsBitField } from 'discord.js'
+import { CommandKit } from 'commandkit'
+import path from 'path'
 
 const client = new Client({
   intents: [
@@ -9,3 +11,17 @@ const client = new Client({
     IntentsBitField.Flags.MessageContent
   ]
 })
+
+new CommandKit({
+  client,
+  commandsPath: path.join(__dirname, 'commands'),
+  eventsPath: path.join(__dirname, 'events'),
+  validationsPath: path.join(__dirname, 'validations'),
+  devGuildIds: ['DEV_SERVER_ID_1', 'DEV_SERVER_ID_2'],
+  devUserIds: ['DEV_USER_ID_1', 'DEV_USER_ID_2'],
+  devRoleIds: ['DEV_ROLE_ID_1', 'DEV_ROLE_ID_2'],
+  skipBuiltInValidations: true,
+  bulkRegister: true
+})
+
+client.login(process.env.TOKEN)
