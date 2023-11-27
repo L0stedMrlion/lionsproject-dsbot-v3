30d088d0a45589d9a9a43f8ce457daa934bffaca
Mrlion <mrlion4202@gmail.com>
2023-11-27 19:13:05 +0100

    added reply function

--- src/commands/informations/server.ts
+++ src/commands/informations/server.ts
@@ -7,7 +7,7 @@ export const data: CommandData = {
 }
 
 export function run({ interaction, client, handler }: SlashCommandProps) {
-  const server = new EmbedBuilder()
+  const stats = new EmbedBuilder()
     .setTitle("🦁 Lion's Project™")
     .setDescription(`You can find here some stats`)
     .setFooter({
@@ -15,4 +15,5 @@ export function run({ interaction, client, handler }: SlashCommandProps) {
       iconURL:
         'https://cdn.discordapp.com/attachments/1092013099168583781/1170441421900218448/lionsproject_logo.png?ex=65590d84&is=65469884&hm=e321b014c27e21524e8efe2b72823971d9dfe8ffff1fedcced5b65391c4816b3'
     })
+  interaction.reply({ embeds: [stats] })
 }
