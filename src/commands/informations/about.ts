cfa7e57e5ccef38a55cb66ff8ba4f6247982f544
Mrlion <mrlion4202@gmail.com>
2023-11-27 15:30:48 +0100

    added footer

--- src/commands/informations/about.ts
+++ src/commands/informations/about.ts
@@ -10,11 +10,13 @@ export function run({ client, interaction }: SlashCommandProps) {
   const about = new EmbedBuilder()
     .setTitle('🦁 LionBot')
     .setDescription(
-      `Hello, I'm LionBot. I'm activly developed by Mrlion and I'm made in discord.js v14.
-      
-      `
+      `Hello, I'm LionBot. I'm activly developed by Mrlion and I'm made in discord.js v14.`
     )
-    .setColor('#ffffff')
+    .setFooter({
+      text: "🦁 Lion's Project™ © 2023",
+      iconURL:
+        'https://cdn.discordapp.com/attachments/1092013099168583781/1170441421900218448/lionsproject_logo.png?ex=65590d84&is=65469884&hm=e321b014c27e21524e8efe2b72823971d9dfe8ffff1fedcced5b65391c4816b3'
+    })
 
   interaction.reply({ embeds: [about] })
 }
