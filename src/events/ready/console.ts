842f3d1e9708b6383f5106ad9d19ec0f89651f37
Mrlion <mrlion4202@gmail.com>
2023-11-28 19:24:45 +0100

    spell check

--- src/events/ready/console.ts
+++ src/events/ready/console.ts
@@ -4,7 +4,7 @@ import { bot_version } from '../../../config.json'
 
 export default function (c: Client<true>, client: Client<true>, handler: CommandKit) {
   console.log(`🦁 LionBot - ${bot_version}`)
-  console.log('🦁 LionBot loading files...')
+  console.log('🦁 LionBot loading all files...')
   console.log('🦁 LionBot is ready!')
   client.user.setActivity("🦁 Lion's Project™", {
     type: ActivityType.Watching
