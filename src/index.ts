69c4b42265a6d1bff29b009b8007738c93da7417
Mrlion <mrlion4202@gmail.com>
2023-11-28 17:17:01 +0100

    some changes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Client, IntentsBitField } from 'discord.js'
 import { CommandKit } from 'commandkit'
 import * as path from 'path'
 import { mrlion_id } from '../config.json'
+const token = process.env.TOKEN
 
 const client = new Client({
   intents: [
@@ -14,20 +15,22 @@ const client = new Client({
 })
 
 new CommandKit({
-  client,
-  commandsPath: path.join(__dirname, 'commands'),
-  eventsPath: path.join(__dirname, 'events'),
-  devUserIds: [mrlion_id],
+  client, // Reference to the Discord client object
+  commandsPath: path.join(__dirname, 'commands'), // Path to the commands directory
+  eventsPath: path.join(__dirname, 'events'), // Path to the events directory
+  devUserIds: [mrlion_id], // Array of developer user IDs
   devRoleIds: [
+    // Array of developer role IDs
     '1092013060983640084', // * Project Founder
     '1176940127499595817', // * Project Team Leader
     '1167143976302805093', // * Project Team Member
     '1092013063089172532', // * Project Support
     '1092013052288835654' // * Administrator (*)
+  ]
 })
 ;(async () => {
   try {
-    await client.login(process.env.TOKEN)
+    await client.login(token)
   } catch (error) {
     console.error('❗ Error', error)
   }
