a572a200487b592970984973457802b22c9774c6
Mrlion <mrlion4202@gmail.com>
2023-11-28 19:24:31 +0100

    added version placehoder

--- src/events/ready/console.ts
+++ src/events/ready/console.ts
@@ -1,7 +1,9 @@
 import { Client, ActivityType } from 'discord.js'
 import type { CommandKit } from 'commandkit'
+import { bot_version } from '../../../config.json'
 
 export default function (c: Client<true>, client: Client<true>, handler: CommandKit) {
+  console.log(`🦁 LionBot - ${bot_version}`)
   console.log('🦁 LionBot loading files...')
   console.log('🦁 LionBot is ready!')
   client.user.setActivity("🦁 Lion's Project™", {
