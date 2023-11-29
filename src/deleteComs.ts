24da0e261332674819c2310461d03230ecfc7317
Mrlion <mrlion4202@gmail.com>
2023-11-29 20:38:33 +0100

    some changes

--- src/deleteComs.ts
+++ src/deleteComs.ts
@@ -1,1 +1,10 @@
+import 'dotenv/config'
 import { REST, Routes } from 'discord.js'
+import { client_id } from '../config.json'
+
+const rest = new REST().setToken('process.env.TOKEN')
+
+rest
+  .put(Routes.applicationCommands(`${client_id}`), { body: [] })
+  .then(() => console.log('✅ Successfully deleted all application commands.'))
+  .catch(console.error)
