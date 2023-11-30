1a5d14e2eaca26545832cbcc1a53e7e5bcb9805d
Mrlion <mrlion4202@gmail.com>
2023-11-30 14:19:53 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import { Client, IntentsBitField } from 'discord.js'
 import { CommandKit } from 'commandkit'
 import * as path from 'path'
-import { version } from '../package.json'
+import { mrlion_id } from '../config.json'
 
 const token = process.env.TOKEN
 
@@ -19,7 +19,7 @@ new CommandKit({
   client,
   commandsPath: path.join(__dirname, 'commands'),
   eventsPath: path.join(__dirname, 'events'),
-  devUserIds: [version],
+  devUserIds: [mrlion_id],
   devRoleIds: [
     '1092013060983640084', // * Project Founder
     '1176940127499595817', // * Project Team Leader
