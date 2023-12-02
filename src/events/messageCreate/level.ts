b2679ed5868de84a3252566dbbcae9c591c01e36
Mrlion <mrlion4202@gmail.com>
2023-12-02 10:21:01 +0100

    removed guild_id; level is now globally

--- src/events/messageCreate/level.ts
+++ src/events/messageCreate/level.ts
@@ -8,4 +8,17 @@ import type { CommandKit } from 'commandkit';
  * @param {Message} message
  */
 
-export default function (c: Client<true>, client: Client<true>, handler: CommandKit) {}
+export default function (
+  c: Client<true>,
+  client: Client<true>,
+  message: Message,
+  handler: CommandKit
+) {
+  if (!message.guild || message.author.bot) {
+    return;
+
+    const query = {
+      userId: message.author.id
+    };
+  }
+}
