3506a61cddee4055413ec7045cfa6dc2aa7dbab5
Mrlion <mrlion4202@gmail.com>
2023-12-02 10:12:28 +0100

    edited console.log

--- src/events/ready/console.ts
+++ src/events/ready/console.ts
@@ -1,9 +1,7 @@
 import { Client, ActivityType } from 'discord.js';
 import type { CommandKit } from 'commandkit';
-import { version } from '../../../package-lock.json';
 
 export default function (c: Client<true>, client: Client<true>, handler: CommandKit) {
-  console.log(`🦁 LionBot - ${version}`);
   console.log('🦁 LionBot loading all files...');
   console.log('🦁 LionBot is ready!');
   client.user.setActivity("🦁 Lion's Project™", {
