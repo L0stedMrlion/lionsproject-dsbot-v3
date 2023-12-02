3579a51cef1d77486d4e4d0e611fbeee10ad344d
Mrlion <mrlion4202@gmail.com>
2023-12-02 10:17:28 +0100

    added level something

---
+++ src/events/messageCreate/level.ts
@@ -0,0 +1,11 @@
+import { Client, Message } from 'discord.js';
+import Level = require('../../schema/level');
+import type { CommandKit } from 'commandkit';
+
+/**
+ *
+ * @param {Client} client
+ * @param {Message} message
+ */
+
+export default function (c: Client<true>, client: Client<true>, handler: CommandKit) {}
